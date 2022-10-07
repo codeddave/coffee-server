3e1aa6c5010d8d2225d1f24839a2945838e18c70
codeddave <drizzzydave@gmail.com>
2022-10-07 05:34:09 +0100

    added Query params to Get

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,19 @@
-import {Body, Controller, Delete, Get, Param, Patch, Post} from "@nestjs/common"
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from "@nestjs/common"
 @Controller("coffees")
 export class CoffeesController {
   @Get()
-  findAll() {
-    return "This action returns all coffees"
+  findAll(@Query() query) {
+    const {limit, offset} = query
+    return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`
   }
   @Get(":id")
   findOne(@Param("id") id: string) {
