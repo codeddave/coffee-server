0649a293377eeeb80158594233fcedf731e79c0a
codeddave <drizzzydave@gmail.com>
2022-10-07 01:04:25 +0100

    using route parameters. handlin request body/ payload

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('coffees')
+import {Body, Controller, Get, Param, Post} from "@nestjs/common"
+@Controller("coffees")
 export class CoffeesController {
   @Get()
   findAll() {
-    return 'This action returns all coffees';
+    return "This action returns all coffees"
+  }
+  @Get(":id")
+  findOne(@Param("id") id: string) {
+    return `This action returns #${id} coffee`
+  }
+  @Post()
+  create(@Body() body) {
+    return body
   }
 }
