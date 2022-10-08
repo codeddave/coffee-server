a94f3d87f6d0fbbcb9338982c1b424508b1c6838
codeddave <drizzzydave@gmail.com>
2022-10-08 11:09:07 +0100

    coffee service update, inject coffee servive into coffee controller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -15,24 +15,24 @@ export class CoffeesController {
 
   @Get()
   findAll(@Query() query) {
-    const { limit, offset } = query
-    return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`
+    //const { limit, offset } = query
+    return this.coffeesService.findAll()
   }
   @Get(":id")
   findOne(@Param("id") id: string) {
-    return `This action returns #${id} coffee`
+    return this.coffeesService.findOne(id)
   }
   @Post()
   create(@Body() body) {
-    return body
+    return this.coffeesService.create(body)
   }
   @Patch(":id")
   update(@Param("id") id: string, @Body() body) {
-    return `This action updates #${id} coffee`
+    return this.coffeesService.update(id, body)
   }
 
   @Delete(":id")
   delete(@Param("id") id: string) {
-    return `This action deletes #${id} coffee`
+    return this.coffeesService.remove(id)
   }
 }
