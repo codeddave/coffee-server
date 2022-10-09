f4c6498e0500f7702ef274018630912e6d117e49
codeddave <drizzzydave@gmail.com>
2022-10-09 06:18:31 +0100

    added an update coffee data transfer object, replaced it with with body for type safety

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -10,6 +10,7 @@ import {
 } from "@nestjs/common"
 import { CoffeesService } from "./coffees.service"
 import { CreateCoffeeDto } from "./dto/create-coffee.dto"
+import { UpdateCoffeeDto } from "./dto/update-coffee.dto"
 @Controller("coffees")
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
@@ -28,8 +29,8 @@ export class CoffeesController {
     return this.coffeesService.create(createCoffeeDto)
   }
   @Patch(":id")
-  update(@Param("id") id: string, @Body() body) {
-    return this.coffeesService.update(id, body)
+  update(@Param("id") id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, updateCoffeeDto)
   }
 
   @Delete(":id")
