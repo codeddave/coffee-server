399c1140614bc338d948ad66e395011dd91a1a86
codeddave <drizzzydave@gmail.com>
2022-10-14 10:51:36 +0100

    schema definition for coffees

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,14 @@
-export type Coffee = {
-  id: number
+import { Prop, Schema } from "@nestjs/mongoose"
+import { Document } from "mongoose"
+
+@Schema()
+export class Coffee extends Document {
+  @Prop()
   name: string
+
+  @Prop()
   brand: string
+
+  @Prop([String])
   flavors: string[]
 }
