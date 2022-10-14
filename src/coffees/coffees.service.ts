1515d2da4925c17111a15b4e0f3e5badb016261d
codeddave <drizzzydave@gmail.com>
2022-10-14 23:05:01 +0100

    updated coffee service, adding interaction with DB

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,49 +1,45 @@
+import { UpdateCoffeeDto } from "./dto/update-coffee.dto"
 import {
   HttpException,
   HttpStatus,
   Injectable,
   NotFoundException,
 } from "@nestjs/common"
+import { InjectModel } from "@nestjs/mongoose"
+import { Model } from "mongoose"
+import { CreateCoffeeDto } from "./dto/create-coffee.dto"
 import { Coffee } from "./entities/coffee.entity"
 
 @Injectable()
 export class CoffeesService {
-  private coffees: Coffee[] = [
-    {
-      id: 1,
-      name: "Shipwreck Roast",
-      brand: "Buddy Brew",
-      flavors: ["chocolate", "vanilla"],
-    },
-  ]
+  constructor(
+    @InjectModel(Coffee.name) private readonly coffeeModel: Model<Coffee>,
+  ) {}
 
-  findAll() {
-    return this.coffees
+  async findAll() {
+    return await this.coffeeModel.find()
   }
 
-  findOne(id: string) {
-    const coffee = this.coffees.find(item => item.id === +id)
+  async findOne(id: string) {
+    const coffee = await this.coffeeModel.findOne({ _id: id })
     if (!coffee) {
       throw new NotFoundException(`Coffee with the id: ${id} `)
     }
     return coffee
   }
 
-  create(createCoffeeDto: any) {
-    this.coffees.push(createCoffeeDto)
+  async create(createCoffeeDto: CreateCoffeeDto) {
+    await this.coffeeModel.create(createCoffeeDto)
   }
 
-  update(id: string, updateCoffeeDto: any) {
-    const existingCoffee = this.findOne(id)
+  async update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+    const existingCoffee = await this.findOne(id)
     if (existingCoffee) {
       // update the existing entity
     }
   }
 
-  remove(id: string) {
-    const coffeeIndex = this.coffees.findIndex(item => item.id === +id)
-    if (coffeeIndex >= 0) {
-      this.coffees.splice(coffeeIndex, 1)
-    }
+  async remove(id: string) {
+    await this.coffeeModel.findByIdAndRemove(id)
   }
 }
