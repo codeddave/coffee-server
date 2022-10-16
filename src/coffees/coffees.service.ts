5d3e63b081b48c6d9295220dcccd230de851084c
codeddave <drizzzydave@gmail.com>
2022-10-16 04:48:35 +0100

    updated coffee entitiy, injected event model into coffee service

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,15 +1,18 @@
 import { UpdateCoffeeDto } from "./dto/update-coffee.dto"
 import { Injectable, NotFoundException } from "@nestjs/common"
-import { InjectModel } from "@nestjs/mongoose"
-import { Model } from "mongoose"
+import { InjectConnection, InjectModel } from "@nestjs/mongoose"
+import { Connection, Model } from "mongoose"
 import { CreateCoffeeDto } from "./dto/create-coffee.dto"
 import { Coffee } from "./entities/coffee.entity"
 import { PaginationQueryDto } from "src/common/dto/pagination-query.dto"
+import { Event } from "src/events/entities/event.entity"
 
 @Injectable()
 export class CoffeesService {
   constructor(
     @InjectModel(Coffee.name) private readonly coffeeModel: Model<Coffee>,
+    @InjectModel(Event.name) private readonly eventModel: Model<Event>,
+    @InjectConnection() private readonly connection: Connection,
   ) {}
 
   async findAll(paginationQuery: PaginationQueryDto) {
