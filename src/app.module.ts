745a782bec7f25aed25256ea23d5c5373f002b0e
codeddave <drizzzydave@gmail.com>
2022-11-08 11:57:50 +0100

    setting up Type ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from "@nestjs/common"
+import { TypeOrmModule } from "@nestjs/typeorm"
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
 import { CoffeesModule } from "./coffees/coffees.module"
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: "",
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
