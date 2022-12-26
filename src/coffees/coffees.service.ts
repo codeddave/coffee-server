7a14a3ad228372885bdfde385f4fe99dcd883c7e
codeddave <drizzzydave@gmail.com>
2022-12-26 13:52:20 +0100

    removing dummy coffeee data, replacing with repository implementation

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -4,33 +4,32 @@ import {
   Injectable,
   NotFoundException,
 } from "@nestjs/common"
+import { InjectRepository } from "@nestjs/typeorm"
+import { Repository } from "typeorm"
+import { CreateCoffeeDto } from "./dto/create-coffee.dto"
 import { Coffee } from "./entities/coffee.entity"
 
 @Injectable()
 export class CoffeesService {
-  private coffees: Coffee[] = [
-    {
-      id: 1,
-      name: "Shipwreck Roast",
-      brand: "Buddy Brew",
-      flavors: ["chocolate", "vanilla"],
-    },
-  ]
-
-  findAll() {
-    return this.coffees
+  constructor(
+    @InjectRepository(Coffee)
+    private readonly coffeeRepository: Repository<Coffee>,
+  ) {}
+  async findAll() {
+    return await this.coffeeRepository.find()
   }
 
-  findOne(id: string) {
-    const coffee = this.coffees.find(item => item.id === +id)
+  async findOne(id: string) {
+    const coffee = await this.coffeeRepository.findOne({ where: { id: +id } })
     if (!coffee) {
       throw new NotFoundException(`Coffee with the id: ${id} `)
     }
     return coffee
   }
 
-  create(createCoffeeDto: any) {
-    this.coffees.push(createCoffeeDto)
+  create(createCoffeeDto: CreateCoffeeDto) {
+    const coffee = this.coffeeRepository.create(createCoffeeDto)
+    return this.coffeeRepository.save(coffee)
   }
 
   update(id: string, updateCoffeeDto: any) {
