904f64527baa674027481a93179533bffbc0209f
codeddave <drizzzydave@gmail.com>
2023-01-06 07:14:50 +0100

    dynamically set a class based on environment

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,16 +5,30 @@ import { CoffeesService } from "./coffees.service"
 import { Coffee } from "./entities/coffee.entity"
 import { Flavor } from "./entities/flavor.entity"
 import { Event } from "src/events/entities/event.entity"
-import { COFFEE_BRANDS } from "./coffees.constants"
+//import { COFFEE_BRANDS } from "./coffees.constants"
 
 /* class MockCoffeeService {} */
+/* class DevelopmentConfigService {}
+class ProductionConfigService {} */
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [
+  /* providers: [
+    CoffeesService,
+    {
+      provide: CoffeesService,
+      useClass:
+        process.env.NODE_ENV === "development"
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
+  ],
+ */
+  /*NON-CLASS-BASED   providers: [
     CoffeesService,
     { provide: COFFEE_BRANDS, useValue: ["buddy brew", "nescafe"] },
-  ],
+  ], */
 
   /*CUSTOM   providers: [{ provide: CoffeesService, useValue: new MockCoffeeService() }], */
   exports: [CoffeesService],
