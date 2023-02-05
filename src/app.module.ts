1a568bedd8edf14d0ab0adae75e9e6bc66eb5d51
codeddave <drizzzydave@gmail.com>
2023-02-06 00:36:24 +0100

    nestjs config set up, schema validation with Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,24 @@ import { CoffeesModule } from "./coffees/coffees.module"
 import { CoffeeRatingModule } from "./coffee-rating/coffee-rating.module"
 import { DatabaseModule } from "./database/database.module"
 import { ConfigModule } from "@nestjs/config"
+import Joi from "@hapi/joi"
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    /*   
+ specify custom path
+ ConfigModule.forRoot({
+      envFilePath: ".environment" you can specify multiple paths by passing an array of strings. 
+    }), */
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+      }),
+    }),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: "postgres",
-
       host: process.env.DATABASE_HOST,
       port: +process.env.DATABASE_PORT,
       username: process.env.DATABASE_USER,
