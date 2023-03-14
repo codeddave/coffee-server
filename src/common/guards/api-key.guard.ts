76a7df5a456a99436b4c6fb546e8f49a4616310f
codeddave <drizzzydave@gmail.com>
2023-03-14 18:18:06 +0100

    bug fix

--- src/common/guards/api-key.guard.ts
+++ src/common/guards/api-key.guard.ts
@@ -17,7 +17,7 @@ export class ApiKeyGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const isPublic = this.reflector.get(IS_PUBLIC_KEY, context.getHandler())
-    if (isPublic) return
+    if (isPublic) return true
     const request = context.switchToHttp().getRequest<Request>()
     const authHeader = request.header("Authorization")
     return authHeader === this.configService.get("API_KEY")
