61b811c694f85ffb90505e2e50f3356202212d38
codeddave <drizzzydave@gmail.com>
2023-03-14 16:51:05 +0100

    moved api key guard into common module, leveraged congisService in guard

--- src/common/guards/api-key.guard.ts
+++ src/common/guards/api-key.guard.ts
@@ -3,19 +3,23 @@ import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common"
 import { Reflector } from "@nestjs/core"
 import { Request } from "express"
 import { Observable } from "rxjs"
+import { ConfigService } from "@nestjs/config"
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
   // The Reflector class allows us to retrieve metadata under a specific context.
 
-  constructor(private readonly reflector: Reflector) {}
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly configService: ConfigService,
+  ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const isPublic = this.reflector.get(IS_PUBLIC_KEY, context.getHandler())
-    if (isPublic) return true
+    if (isPublic) return
     const request = context.switchToHttp().getRequest<Request>()
     const authHeader = request.header("Authorization")
-    return authHeader === process.env.API_KEY
+    return authHeader === this.configService.get("API_KEY")
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { HttpExceptionFilter } from "./common/filters/http-exception.filter"
 import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
-import { ApiKeyGuard } from "./common/guards/api-key.guard"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -16,8 +15,6 @@ async function bootstrap() {
       } automatically converts types instead of manually doing conversions using @Type(()=> ) */
     }),
   )
-  app.useGlobalFilters(new HttpExceptionFilter()),
-    app.useGlobalGuards(new ApiKeyGuard())
-  await app.listen(4000)
+  app.useGlobalFilters(new HttpExceptionFilter()), await app.listen(4000)
 }
 bootstrap()
