ae8e3e8f5779268f131df5311bcc5a45ff7ec7f0
codeddave <drizzzydave@gmail.com>
2023-03-15 18:07:41 +0100

    bug fix

--- src/common/interceptors/wrap-response.interceptor.ts
+++ src/common/interceptors/wrap-response.interceptor.ts
@@ -10,10 +10,11 @@ import { map, Observable } from "rxjs"
 export class WrapResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     console.log("Before...")
+
     return next.handle().pipe(
-      map(data => {
-        data
-      }),
+      map(data => ({
+        data,
+      })),
     )
   }
 }
