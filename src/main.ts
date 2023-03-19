4267a2766f37b0218fa4372ac98c99f61272357c
codeddave <drizzzydave@gmail.com>
2023-03-19 23:13:40 +0100

    adding Swagger to handle documentation

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { HttpExceptionFilter } from "./common/filters/http-exception.filter"
 import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -22,6 +23,14 @@ async function bootstrap() {
       new WrapResponseInterceptor(),
       new TimeoutInterceptor(),
     )
+
+  const options = new DocumentBuilder()
+    .setTitle("Coffees-server")
+    .setDescription("server")
+    .setVersion("1.0")
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup("api", app, document)
   await app.listen(4000)
 }
 bootstrap()
