72fc4c462812165e76c6439f3de93c50101e4092
codeddave <drizzzydave@gmail.com>
2023-03-21 22:20:40 +0100

    updates

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,4 +1,4 @@
+import { PartialType } from "@nestjs/swagger"
 import { CreateCoffeeDto } from "./create-coffee.dto"
-import { PartialType } from "@nestjs/mapped-types"
 
 export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
